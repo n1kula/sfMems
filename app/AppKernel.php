2467828d9e8ab28486c9a97e2565965a03edef2d
Tomek <tomek@kulesza>
2014-11-01 13:24:24 +0100

    doctrine/migrations installed + db migrate

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,7 @@ class AppKernel extends Kernel
             new Mems\UserBundle\MemsUserBundle(),
             new Mems\MemsBundle\MemsMemsBundle(),
             new Mems\LayoutBundle\MemsLayoutBundle(),
+            new Doctrine\Bundle\MigrationsBundle\DoctrineMigrationsBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
